dad63f21440d594511dc6e2e912ead799710016b
hadil555 <farahhadil33@gmail.com>
2024-12-19 12:38:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const scategorieRouter =require("./routes/scategorie.route")
+const dotenv=require("dotenv")
+const cors=require("cors")
+const app=express();
+const mongoose=require("mongoose")
+const categorie = require("./models/categorie")
+const categorieRouter = require("./routes/categorie.route")
+const articleRouter =require("./routes/article.route")
+dotenv.config()
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+
+res.send("page acceuil")
+})
+// Connexion à la base données
+mongoose.connect(process.env.DATABASECLOUD)
+.then(() => {console.log("DataBase Successfully Connected");})
+.catch(err => { console.log("Unable to connect to database", err);
+process.exit(); });
+app.use("/api/categorie", categorieRouter)
+app.use('/api/scategorie', scategorieRouter);
+app.use('/api/article', articleRouter);
+
+
+app.listen(process.env.PORT,()=>
+console.log(`app executer sur le port ${process.env.PORT}`))
+module.exports=app;
